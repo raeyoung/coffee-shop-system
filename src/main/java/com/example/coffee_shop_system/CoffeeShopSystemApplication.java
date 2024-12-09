7f359dcac92cf07b8bdff9c815c3ad0feeb3dfd9
raeyoung <raeyoung233@gmail.com>
2024-12-09 09:24:20 +0900

    first commit

---
+++ src/main/java/com/example/coffee_shop_system/CoffeeShopSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffee_shop_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeShopSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeShopSystemApplication.class, args);
+	}
+
+}
